b02952182141176d34d81cf360397893c2946e36
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-25 13:17:42 +0100

    made the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 import { ClearButton, GradientButton } from "./reuseableComp/Button";
 
 const Navbar = () => {
+  const [mobileDrawer, setMobileDrawerOpen] = useState(false);
+
+  const toogleNavBar = () => {
+    setMobileDrawerOpen(!mobileDrawer);
+  };
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
@@ -22,7 +30,27 @@ const Navbar = () => {
             <ClearButton text="Sign in" />
             <GradientButton text=" Create an Account" />
           </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toogleNavBar}>
+              {mobileDrawer ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
+        {mobileDrawer && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <ClearButton text="Sign in" />
+              <GradientButton text=" Create an Account" />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
