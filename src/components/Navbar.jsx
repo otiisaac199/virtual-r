49085a1ec1385c847106c3b77b564e4a92aaf85d
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-25 11:08:40 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-center">
+          <div className="flex items-center flex-shrink-0"></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
